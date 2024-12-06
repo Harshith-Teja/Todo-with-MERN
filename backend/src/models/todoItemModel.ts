4d85f7cf4bbcc017fdfc12b1d07a7af0dbff677a
Harshith Teja <harshithtejadontoju@gmail.com>
2024-12-06 19:34:36 +0530

    built todoItemModel

---
+++ backend/src/models/todoItemModel.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+export type TodoItemType = {
+  id: number;
+  item: string;
+  checked: boolean;
+};
+
+const todoItemSchema = new Schema(
+  {
+    id: { type: Number },
+    item: { type: String },
+    checked: { type: Boolean },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<TodoItemType>("TodoItem", todoItemSchema);
