afef6b1e171d44fde80b895565a28c33a47196f9
Harshith Teja <harshithtejadontoju@gmail.com>
2024-12-06 19:28:26 +0530

    added model, config, routes, controller folders and their corresponding files in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use("/api/tasks", taskRoutes);
+
+//DB connection
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
